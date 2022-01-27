9598c3c6f98277089323f25f7985e201444283a3
david doria <dor15004@byui.edu>
2022-01-26 17:27:58 -0700

    Added Team Activity Code

--- util/database.js
+++ util/database.js
@@ -1,10 +1,31 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
+const cors = require('cors');
 
 let _db;
 
+
+/* Not Part of Academind. This is for Heroku */
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://Skhoooler:347821597dD@backend-web-dev-ii.zxs2i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const corsOptions = {
+  origin: "doria-web-backend-dev-2.herokuapp.com",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+};
+/* Not Part of Academind. This is for Heroku */
+
+
 const mongoConnect = (callBack) => {
-  MongoClient.connect('mongodb+srv://Skhoooler:347821597dD@backend-web-dev-ii.zxs2i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  MongoClient.connect(MONGODB_URL, options)
   .then(client => {
     console.log('MongoDB Connected');
 
