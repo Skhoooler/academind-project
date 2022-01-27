92eaa34107c1c43c9b6c587ed9903a2ea76e7769
david doria <dor15004@byui.edu>
2022-01-26 17:13:35 -0700

    Academind Project

---
+++ models/product.js
@@ -0,0 +1,40 @@
+const getDb = require('../util/database').getDb;
+
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection('products')
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(`Database Error: ${error}`)
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log("Products Returned");
+        return products;
+      })
+      .catch(err => {
+      console.log(`Database Error: ${err}`);
+    });
+  }
+}
+
+module.exports = Product;
